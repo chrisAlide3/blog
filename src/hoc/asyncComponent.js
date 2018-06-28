e709e88223a2eb692b48838c04395e1c75707874
chris <christian.bracher@gmail.com>
2018-06-28 12:38:26 +0800

    asyncComponent working

---
+++ src/hoc/asyncComponent.js
@@ -0,0 +1,29 @@
+// Higher Order component for loading Javascript on the fly (lazy loading) when used and not all the scripts at the start. Used in big applications
+
+import React, { Component } from 'react';
+
+const asyncComponent = (ImportComponent) => {
+
+    return class extends Component {
+        state = {
+            component: null,
+        }
+
+        componentDidMount () {
+            ImportComponent()
+                .then(cmp => {
+                    this.setState({component: cmp.default});
+                })
+        }
+
+        render () {
+
+            const C = this.state.component;
+
+            return C ? <C {...this.props} /> : null
+
+        }
+    }
+}
+
+export default asyncComponent;
