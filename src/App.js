aa22d6b2db3c1417f71a0a857cc72234751fdb17
chris <christian.bracher@gmail.com>
2018-06-28 13:04:05 +0800

    async loading of javascrip (lazy loading) and basename

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+
+      // If your website root doesn't start at www.example.com/ so the "/" you can define a basename on the BrowserRouter
+      // <BrowserRouter basename="/posts">
+
       <BrowserRouter>
         <div className="App">
           <Blog />
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
+import asyncComponent from '../../hoc/asyncComponent'
+
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
 // import FullPost from './FullPost/FullPost';
 
+const AsyncNewPost = asyncComponent(()=>{
+    return import('./NewPost/NewPost');
+}) 
+
 class Blog extends Component {
+    state = {
+        signedIn: true,
+    }
 
     render () {
 
@@ -41,6 +50,7 @@ class Blog extends Component {
                  {/* With component we render a React component */}
 
                  {/* Here we redirect the root path "/" to another path "/posts" */}
+
                 {/* <Route path="/" exact render={() => (
                     <Redirect to="/posts" /> )}/>
                 <Route path="/new-post" exact component={NewPost} />
@@ -48,11 +58,21 @@ class Blog extends Component {
 
                 {/* With switch the 1st child that match path get rendered, others don't. The order is important! */}
                 <Switch>
-                    <Route path="/new-post" exact component={NewPost} />
+                    {/* We can use Route to forbid access to a page with conditions, like here. Or we can make the check in ComponentDidMount of the Target route to Push back through History Props of the route */}
+                    {/* {this.state.signedIn ?<Route path="/new-post" exact component={NewPost} /> : null} */}
+
+                    {/* Here we call AsyncNewPost for javascript lazy loading. See higher order component 'asyncComponent' */}
+                    {this.state.signedIn ?<Route path="/new-post" exact component={AsyncNewPost} /> : null}
+
+
+                    {/* <Route path="/new-post" exact component={NewPost} /> */}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
+                    {/* <Route path="/posts/:id" exact component={FullPost} /> */}
 
-                {/* <Route path="/posts/:id" exact component={FullPost} /> */}
+                    {/* To catch 404 messages, Page not found. Just use <Route without a path. Always put it last. It doesn't work on the root / path if redirect has been defined. In this case it will only work on sub-directories */}
+                    {/* <Route render={()=> <h1>Page not found</h1>}/> */}
+
                 </Switch>
 
 
